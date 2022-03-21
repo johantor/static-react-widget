6bcef6a3d5f94d137083762a0e4d7d36062c7fc8
Johan Tor <johan.tor@gmail.com>
2022-03-21 11:46:30 +0100

    Fix netlify deploy (#1)

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,13 +1,19 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const URL = "https://catfact.ninja/fact";
-const file = "./public/catfact.json";
+const file = `public/catfact.json`;
 
 axios
     .get(URL)
     .then((response) => {
         response.data.time = new Date();
         fs.outputFile(file, JSON.stringify(response.data));
+        console.log(
+            "Writing data to file (",
+            file,
+            "): ",
+            JSON.stringify(response.data)
+        );
     })
     .catch((_err) => {
         //console.log(err);
